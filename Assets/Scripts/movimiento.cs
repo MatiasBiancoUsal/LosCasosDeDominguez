fba1936158547f0ccaaec71293bd5ab1e0e16c7b
PilarGarcia00 <garcia.p@usal.edu.ar>
2026-06-03 20:19:56 -0300

    Código de efecto click

--- Assets/Scripts/movimiento.cs
+++ Assets/Scripts/movimiento.cs
@@ -6,6 +6,10 @@ public class movimiento : MonoBehaviour
 {
     [SerializeField] private float speed;
 
+    [Header("Efecto de Clic")]
+    [SerializeField] private GameObject efectoClicPrefab; 
+    [SerializeField] private float tiempoDeVidaEfecto = 0.5f;
+
     private Vector2 target;
     private Camera Cam;
 
@@ -21,9 +25,15 @@ public class movimiento : MonoBehaviour
         {
             Vector3 worldPoint = Cam.ScreenToWorldPoint(Input.mousePosition);
             target = new Vector2(worldPoint.x, worldPoint.y);
+
+            if (efectoClicPrefab != null)
+            {
+                GameObject nuevoEfecto = Instantiate(efectoClicPrefab, new Vector3(target.x, target.y, 0f), Quaternion.identity);
+
+                Destroy(nuevoEfecto, tiempoDeVidaEfecto);
+            }
         }
 
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
-
 }
