20da9064b0dbcefd0f2494c44fe8208680d233b2
PilarGarcia00 <garcia.p@usal.edu.ar>
2026-06-06 12:10:21 -0300

    Panel inventario y sospechosos
    Agregué que solo funcione si haces hover y que se pare el juego

--- Assets/Scripts/Inventarios/Inventarios.cs
+++ Assets/Scripts/Inventarios/Inventarios.cs
@@ -1,20 +1,52 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Inventarios : MonoBehaviour
 {
     [SerializeField] private GameObject PanelInvPistas;
     [SerializeField] private GameObject PanelInvSospechosos;
 
+    private bool mouseEncima = false;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (mouseEncima && Keyboard.current != null)
         {
-            PanelInvPistas.SetActive(!PanelInvPistas.activeSelf);
-        }
+            if (Keyboard.current.wKey.wasPressedThisFrame)
+            {
+                PanelInvPistas.SetActive(!PanelInvPistas.activeSelf);
+                ActualizarEstadoTiempo(); //si se pausa o despausa
+            }
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            PanelInvSospechosos.SetActive(!PanelInvSospechosos.activeSelf);
+            if (Keyboard.current.sKey.wasPressedThisFrame)
+            {
+                PanelInvSospechosos.SetActive(!PanelInvSospechosos.activeSelf);
+                ActualizarEstadoTiempo(); 
+            }
         }
+    }
+
+    private void ActualizarEstadoTiempo()
+    {
+        if (PanelInvPistas.activeSelf || PanelInvSospechosos.activeSelf)
+        {
+            Time.timeScale = 0f; // Pausa el juego
+            Debug.Log("Juego Pausado");
+        }
+        else
+        {
+            Time.timeScale = 1f; // Reanuda el juego
+            Debug.Log("Juego Reanudado");
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEncima = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseEncima = false;
     }
 }
