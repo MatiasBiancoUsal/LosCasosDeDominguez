18499f5cea94c3b2be39f1042132f524ff92e43c
PilarGarcia00 <garcia.p@usal.edu.ar>
2026-06-11 13:08:35 -0300

    CINEMATICA2
    Lo hizo cande pero no tiene compu!!!!!
    
    Co-Authored-By: godoymacielcandelaria-tech <279167111+godoymacielcandelaria-tech@users.noreply.github.com>

--- Assets/Scripts/Multimedia/ActivadorCinematicas.cs
+++ Assets/Scripts/Multimedia/ActivadorCinematicas.cs
@@ -3,16 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ActivadorCinematicas : MonoBehaviour
 {
-    
     public Animator anim;
     public Canvas canva;
+    public int escenaDestino;
 
     void Start()
     {
-        
+
     }
+
     public void termina()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(escenaDestino);
     }
 }
