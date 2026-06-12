9b0841fe83d8b3917cedefd29eaa49a3b57bfa09
LucianaVMN <l.mazzolenisnoe@usal.edu.ar>
2026-06-11 22:03:02 -0300

    Arreglo ? Panel Inspección sospechosos

--- Assets/Scripts/Inspeccion/ClickParteCuerpo.cs
+++ Assets/Scripts/Inspeccion/ClickParteCuerpo.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class ClickParteCuerpo : MonoBehaviour
 {
+    [Header("Configuración de la Pista")]
     [SerializeField] private DesbloquearPistaTexto textoADesbloquear;
 
-    private void OnMouseDown()
+    public void RegistrarClicParteCuerpo()
     {
-         Debug.Log("CLICK DETECTADO");
-         
+        Debug.Log("¡CLICK DETECTADO EN LA PARTE DEL CUERPO: " + gameObject.name + "!");
+
         if (textoADesbloquear != null)
         {
             textoADesbloquear.Desbloquear();
+        }
+        else
+        {
+            Debug.LogWarning("Falta asignar el script de texto en " + gameObject.name);
         }
     }
 }
--- Assets/Scripts/Inspeccion/DesbloquearPistaTexto.cs
+++ Assets/Scripts/Inspeccion/DesbloquearPistaTexto.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class DesbloquearPistaTexto : MonoBehaviour
 {
+    [Header("Componentes de UI")]
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private float velocidadFade = 3f;
 
     private bool desbloqueado = false;
+
+    private void Start()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0f;
+        }
+    }
 
     private void Update()
     {
