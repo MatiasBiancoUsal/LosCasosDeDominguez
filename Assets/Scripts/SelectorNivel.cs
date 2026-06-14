ea1ac9d0a53840b16f54d4d6004034f28f2546b3
PilarGarcia00 <garcia.p@usal.edu.ar>
2026-06-14 19:54:11 -0300

    Mapa
    Script de botones que te llevan a las escenas

---
+++ Assets/Scripts/SelectorNivel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SelectorNivel : MonoBehaviour
+{
+    [Header("Configuración del Nivel")]
+    [Tooltip("Escribe aquí el nombre exacto de la escena a la que lleva este objeto")]
+    public string nombreDeLaEscena;
+
+    public void CargarNivelBoton()
+    {
+        if (!string.IsNullOrEmpty(nombreDeLaEscena))
+        {
+            SceneManager.LoadScene(nombreDeLaEscena);
+        }
+    }
+}
