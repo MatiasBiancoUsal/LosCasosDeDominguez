b41be664f658fe88437d7d7360318c2560ec3cd5
godoymacielcandelaria-tech <godoymaciel.candelaria@usal.edu.ar>
2026-06-24 23:39:09 -0300

    pruebas para arreglar a elisa

--- Assets/Assets/Mecanica inspeccion/DetectorHover.cs
+++ Assets/Assets/Mecanica inspeccion/DetectorHover.cs
@@ -2,38 +2,54 @@ using UnityEngine;
 
 public class DetectorHover : MonoBehaviour
 {
+    [Header("Configuración")]
     public GameObject botonFlotante;
+    public LayerMask interactableLayer;
 
     private bool mouseEstaEncima = false;
 
     // Permite que otros scripts sepan si el mouse está sobre este objeto
     public bool MouseEstaEncima => mouseEstaEncima;
 
-
-    private void OnMouseEnter()
-
-    {
-        Debug.Log("ENTER DETECTOR HOVER ELISA");
-        if (botonFlotante != null)
-        {
-            botonFlotante.SetActive(true);
-        }
-
-        mouseEstaEncima = true;
-    }
-
-    private void OnMouseExit()
-    {
-        if (botonFlotante != null)
-        {
-            botonFlotante.SetActive(false);
-        }
-
-        mouseEstaEncima = false;
-    }
-
     private void Update()
     {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Collider2D[] hits = Physics2D.OverlapPointAll(mousePos);
+
+        bool sobreMi = false;
+
+        foreach (Collider2D hit in hits)
+        {
+            // Ignora todo lo que no sea Interactable
+            if (((1 << hit.gameObject.layer) & interactableLayer) == 0)
+                continue;
+
+            if (hit.gameObject == gameObject)
+            {
+                sobreMi = true;
+                break;
+            }
+        }
+
+        // Entró al hover
+        if (sobreMi && !mouseEstaEncima)
+        {
+            mouseEstaEncima = true;
+
+            if (botonFlotante != null)
+                botonFlotante.SetActive(true);
+        }
+
+        // Salió del hover
+        if (!sobreMi && mouseEstaEncima)
+        {
+            mouseEstaEncima = false;
+
+            if (botonFlotante != null)
+                botonFlotante.SetActive(false);
+        }
+
         // Si el mouse está encima y apretás Q
         if (mouseEstaEncima && Input.GetKeyDown(KeyCode.Q))
         {
